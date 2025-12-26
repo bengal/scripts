8b94fcdd43283fdedf89caf791bf1a9e0fa55611
Beniamino Galvani <bgalvani@redhat.com>
2025-12-26 16:26:52 +0100

    nat64: fix fragmentation

--- nat64/nat64-v2.sh
+++ nat64/nat64-v2.sh
@@ -18,7 +18,7 @@ IFACE=enp7s0
 #           v                                           [ns1] #
 #                                                             #
 #         veth1      <--->  nat64  <--->     dummy1           #
-u#    2002:aaaa::1/64       (tayga)       100.25.1.1/24        #
+#    2002:aaaa::1/64       (tayga)       100.25.1.1/24        #
 #        (radvd)                               ^              #
 #                                              |              #
 #                                            (nat)            #
@@ -109,6 +109,7 @@ ipv4-addr 100.25.1.133
 prefix 64:ff9b::/96
 dynamic-pool 100.25.1.144/28
 data-dir /var/lib/tayga/nat64
+offlink-mtu 1492
 EOF
 ip netns exec ns1 tayga --config /tmp/tayga.conf --mktun
 ip -n ns1 link set nat64 up
